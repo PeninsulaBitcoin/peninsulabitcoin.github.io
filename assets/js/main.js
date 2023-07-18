db029ac1af57489832f16b9243e3c7a4f5baf3ff
Ian <ian.downey@gmail.com>
2023-07-18 20:50:49 +1000

    Addition of Resources Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,15 +83,20 @@
    */
   let selectHeader = select("#header");
   if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add("header-scrolled");
-      } else {
-        selectHeader.classList.remove("header-scrolled");
-      }
-    };
-    window.addEventListener("load", headerScrolled);
-    onscroll(document, headerScrolled);
+    var page = location.pathname.substring(
+      location.pathname.lastIndexOf("/") + 1
+    );
+    if (page == "index.html") {
+      const headerScrolled = () => {
+        if (window.scrollY > 100) {
+          selectHeader.classList.add("header-scrolled");
+        } else {
+          selectHeader.classList.remove("header-scrolled");
+        }
+      };
+      window.addEventListener("load", headerScrolled);
+      onscroll(document, headerScrolled);
+    }
   }
 
   /**
